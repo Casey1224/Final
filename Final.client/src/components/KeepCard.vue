47cefb04f8299fd52ec17d639d519dcd0c0c3516
Casey Collins <collinscasey.homes@gmail.com>
2022-09-27 14:29:18 -0600

    comi

---
+++ Final.client/src/components/KeepCard.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <router-link :to="{ name: 'KeepDetails', params: { keepId: keep.id } }">
+        <div class="bg-light border elevation-2 rounded selectable">
+            <div class="p-1">
+                <img class="img-fluid" :src="keep.img" alt="">
+                <h6 class="text-dark"><b>{{ keep.name }}</b></h6>
+                <img :src="keep.creator.img" class="border border-circle" alt="">
+            </div>
+        </div>
+    </router-link>
+
+
+
+
+</template>
+<script>
+import { router } from '../router.js'
+export default {
+    props: { keep: { type: Object, required: true } },
+    setup(props) {
+        return {
+            goTo() {
+                router.push({ name: 'KeepsDetailsPage', params: { keepId: props.keep.id } })
+            }
+
+
+
+        };
+    },
+};
+</script>
+<style >
+
+</style>
--- Final.client/src/router.js
+++ Final.client/src/router.js
@@ -21,7 +21,13 @@ const routes = [
     name: 'Account',
     component: loadPage('AccountPage'),
     beforeEnter: authGuard
-  }
+  },
+  {
+    path: '/keep/:keepId',
+    name: 'KeepDetails',
+    component: loadPage('KeepDetailsPage'),
+    beforeEnter: authSettled
+  },
 ]
 
 export const router = createRouter({
